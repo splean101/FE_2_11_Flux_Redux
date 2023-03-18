721e4848d339dfddcb24ea497a51695a328db9d8
splean101 <101splean@gmail.com>
2023-03-18 14:02:22 +0200

    2nd done

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,34 +4,46 @@ import { useDispatch, useSelector } from 'react-redux';
 const Products = () => {
 
   const coffee = useSelector((state) => state.coffee)
+  const sugar = useSelector((state) => state.sugar)
   const isLoggedIn = useSelector((state) => state.isLoggedIn)
 
   const dispatchFunc = useDispatch();
 
-  const increment = () => {
-    dispatchFunc({ type: 'incrementCoffee' })
+  const incrementCoffee = () => {
+    dispatchFunc({ type: 'INCREMENT_COFFEE' })
   }
-  const decrement = () => {
-    dispatchFunc({ type: 'decrementCoffee' })
+  const decrementCoffee = () => {
+    dispatchFunc({ type: 'DECREMENT_COFFEE' })
   }
 
-  const increase = () => {
-    dispatchFunc({ type: 'increase', number: 10 })
+  const increaseCoffee = () => {
+    dispatchFunc({ type: 'INCREASE_COFFEE', number: 10 })
+  }
+
+  const incrementSugar = () => {
+    dispatchFunc({ type: 'INCREMENT_SUGAR' })
+  }
+  const decrementSugar = () => {
+    dispatchFunc({ type: 'DECREMENT_SUGAR' })
+  }
+
+  const increaseSugar = () => {
+    dispatchFunc({ type: 'INCREASE_SUGAR', number: 10 })
   }
 
   return (
     <>
       <div className='product'>
         <span>{`Coffee: ${coffee}`}</span>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
-        {isLoggedIn && <button onClick={increase}>+10</button>}
+        <button onClick={incrementCoffee}>+</button>
+        <button onClick={decrementCoffee}>-</button>
+        {isLoggedIn && <button onClick={increaseCoffee}>+10</button>}
       </div>
       <div className='product'>
-        <span>{`Sugar: 0`}</span>
-        <button>+</button>
-        <button>-</button>
-        {isLoggedIn && <button onClick={increase}>+10</button>}
+        <span>{`Sugar: ${sugar}`}</span>
+        <button onClick={incrementSugar}>+</button>
+        <button onClick={decrementSugar}>-</button>
+        {isLoggedIn && <button onClick={increaseSugar}>+10</button>}
       </div>
     </>
   )
--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,39 @@
-import { legacy_createStore } from 'redux'
+import { legacy_createStore } from 'redux';
 
-const couterReducer = (state = {coffee: 0, isLoggedIn: false}, action) => {
-    if (action.type === 'incrementCoffee'){
-        return {
-           coffee: state.coffee + 1,
-           isLoggedIn: state.isLoggedIn,
-        }
-    }
-    if (action.type === 'decrementCoffee'){
-        return {
-           coffee: state.coffee - 1,
-           isLoggedIn: state.isLoggedIn,
-        }
-    }
-    if (action.type === 'increase'){
-        return {
-           coffee: state.coffee + action.number,
-           isLoggedIn: state.isLoggedIn,
-        }
-    }
-    if(action.type === 'logIn'){
-        return {
-            coffee: state.coffee,
-            isLoggedIn: true,
-         }
-    }
-    return state
-}
+const couterReducer = (
+  state = { coffee: 0, sugar: 0, isLoggedIn: false },
+  action
+) => {
+  const INCREMENT_COFFEE = 'INCREMENT_COFFEE';
+  const DECREMENT_COFFEE = 'DECREMENT_COFFEE';
+  const INCREASE_COFFEE = 'INCREASE_COFFEE';
 
-const store = legacy_createStore(couterReducer); 
+  const INCREMENT_SUGAR = 'INCREMENT_SUGAR';
+  const DECREMENT_SUGAR = 'DECREMENT_SUGAR';
+  const INCREASE_SUGAR = 'INCREASE_SUGAR';
+
+  const LOGIN = 'LOGIN';
+
+  switch (action.type) {
+    case INCREMENT_COFFEE:
+      return { ...state, coffee: state.coffee + 1 };
+    case DECREMENT_COFFEE:
+      return { ...state, coffee: state.coffee - 1 };
+    case INCREASE_COFFEE:
+      return { ...state, coffee: state.coffee + action.number };
+    case INCREMENT_SUGAR:
+      return { ...state, sugar: state.sugar + 1 };
+    case DECREMENT_SUGAR:
+      return { ...state, sugar: state.sugar - 1 };
+    case INCREASE_SUGAR:
+      return { ...state, sugar: state.sugar + action.number };
+    case LOGIN:
+      return { ...state, isLoggedIn: true };
+  }
+
+  return state;
+};
+
+const store = legacy_createStore(couterReducer);
 
 export default store;
